69aac8df58f0d12ac5eb3cf0bd85fede923f16da
Jessi <jcsoftwareengineer369@gmail.com>
2025-07-22 20:59:26 -0700

    tailwindcss is now working. Lets check it out

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-900 to-gray-800">
+<h1 className="text-4xl font-mono font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-blue-500 to-red-500 tracking-wider text-center">
+        Hello! <span className="animate-pulse text-white drop-shadow-lg">TailwindCSS</span> is officially connected and ready to go!
+      </h1>
     </div>
   );
 }
